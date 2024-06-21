b92a029b610b7bb1c17f3f885f399d8a81b49718
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-06-22 00:36:55 +0700

    build base project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {useRef} from 'react'
 import './App.css';
+import Weather  from './components/Weather';
 
 function App() {
+
+  const nameApp = useRef('Weather App - Tekko')
+  document.title = nameApp.current
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Weather />
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './weather.css'
+
+const Weather = () => {
+    return (
+        <div className='weather'>
+            
+
+        </div>
+    )
+}
+
+export default Weather
